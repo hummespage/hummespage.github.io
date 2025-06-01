b8e8196058f5b7fb26bcc1695a11d48e6ce76932
hummespage <samuel.schuler2@gmail.com>
2025-06-01 18:54:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// Função para ligar/desligar a música de fundo
 function toggleMusica() {
   const musica = document.getElementById("musica");
 
-  // Define volume inicial
   musica.volume = 0.5;
 
-  // Tenta reproduzir caso esteja pausada
   if (musica.paused) {
     musica.play().catch((err) => {
-      console.log("O navegador bloqueou o autoplay. Clique para liberar o som.");
+      console.log("Navegador bloqueou autoplay. Clique para liberar o som.");
     });
   } else {
     musica.pause();
