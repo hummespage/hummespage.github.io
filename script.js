b6dad64370f8af88e97da7d39773d9e3412e5e48
hummespage <samuel.schuler2@gmail.com>
2025-06-01 19:08:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function toggleMusica() {
   const musica = document.getElementById("musica");
-  musica.volume = 0.4;
+
+  // Garante volume e ativa o som
+  musica.volume = 0.5;
+  musica.muted = false;
 
   if (musica.paused) {
-    musica.play().catch(err => {
-      console.log("Clique necessário para liberar o som.");
+    musica.play().catch((err) => {
+      console.log("Clique necessário para ativar o som:", err);
     });
   } else {
     musica.pause();
